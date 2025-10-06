53707e0fe693f1ba93d3b0d50a9acd5477cf707f
Vlad Mendel <vladmendel@ukr.net>
2025-10-06 17:25:37 -0500

    feat: integrate Argos CI for visual testing
    - Added @argos-ci/playwright as a dev dependency to enable visual testing.
    - Updated playwright.config.ts to configure multiple reporters including Argos CI reporter.
    - Modified firstTest.spec.ts to capture a screenshot of the menu page using Argos CI.

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from "@playwright/test";
 import { MainPage } from "../pages/MainPage";
+import { argosScreenshot } from "@argos-ci/playwright";
 
 test('first test', async ({page}) => {
     
     const mainPage = new MainPage(page);
 
     await mainPage.navigateToMenuPage()
+    await argosScreenshot(page, "menu_page")
 })
